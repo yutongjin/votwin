d1bd2eddc0fa44bee660f8b238733e74211de2bd
Ricky Huang <ricky_huang@intuit.com>
2021-07-22 10:03:16 -0700

    Add placeholder for registration / vote / election creation

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ function App() {
     <Router>
       <nav><ul>
         <li><Link to="/home">Home</Link></li>
+        <li><Link to="/register">Register Voters</Link></li>
+        <li><Link to="/vote">Vote</Link></li>
+        <li><Link to="/election">Create Elections</Link></li>
         <li><Link to="/userTools">User Tools</Link></li>
       </ul></nav>
       <Switch>
         <Route path="/home"><div></div></Route>
         <Route path="/userTools"><UserTool /></Route>
+        <Route path="/register"><div>register</div></Route>
+        <Route path="/vote"><div>vote</div></Route>
+        <Route path="/election"><div>election</div></Route>
       </Switch>
     </Router>
   );
