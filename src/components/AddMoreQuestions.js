5134ff63ff965ae21daf9ac094b03988c9affb95
cshu <cassie_shu@intuit.com>
2021-07-22 23:04:14 -0700

    finished the UI part, need assistance with db

--- src/components/AddMoreQuestions.js
+++ src/components/AddMoreQuestions.js
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import SaveElection from './SaveElection.js';
 
 function AddMoreQuestions() {
     const [questionTitle, setQuestionTitle] = useState('');
-    let [questionList, setQuestionList] = useState([]);
+    const [questionList, setQuestionList] = useState([]);
 
     const handleChange = (e) => {
         setQuestionTitle(e.target.value);
@@ -16,6 +15,10 @@ function AddMoreQuestions() {
         console.log(questionList);
     }
 
+    const handleSaveElection = () => {
+        
+    }
+
     return (
         <div>
             <form>
@@ -24,10 +27,37 @@ function AddMoreQuestions() {
                     <input type="text" onChange={handleChange}/><br />
                     <button text="Add question" onClick={handleAdd}> Add Question </button>
                 </label>
-                <SaveElection />
+                <button text="Save Election" onClick={handleSaveElection}> Save Election </button>
             </form>
         </div>
     )
 };
 
 export default AddMoreQuestions;
+
+// This one is called from our UI, eg <CarForm onAdd{addCar} />
+// export function addCar(car) {
+//     return (dispatch) => {
+//       dispatch(createAddCarRequestAction());
+//       return fetch("http://localhost:3060/cars", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(car),
+//       }).then(() => dispatch(refreshCars()));
+//     };
+//   }
+//   // These two are action creators, so you can dispatch() them:
+//   export const createAddCarRequestAction = () => ({
+//     type: ADD_CAR_REQUEST_ACTION,
+//   });
+//   // This is called once we've added the car. As you can see in addCar(),
+//   // when the POST is done, we then can get the latest list of cars from
+//   // the server.
+//   export const refreshCars = () => {
+//     return (dispatch) => {
+//       dispatch(createRefreshCarsRequestAction());
+//       return fetch("http://localhost:3060/cars")
+//         .then((res) => res.json())
+//         .then((cars) => dispatch(createRefreshCarsDoneAction(cars)));
+//     };
+//   };
